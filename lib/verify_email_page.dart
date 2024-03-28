7b3265383aa6c624aa92baaaef01144953f8c9a9
Austin <105258719+Austin-540@users.noreply.github.com>
2024-03-29 09:48:36 +1300

    page to tell user to verify their email

---
+++ lib/verify_email_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'globals.dart';
+import 'home_page.dart';
+
+class VerifyEmailPage extends StatefulWidget {
+  const VerifyEmailPage({super.key, required this.email});
+  final String email;
+
+  @override
+  State<VerifyEmailPage> createState() => _VerifyEmailPageState();
+}
+
+class _VerifyEmailPageState extends State<VerifyEmailPage> {
+  Future isVerified = pb.collection('users').getOne(pb.authStore.model.id, fields: "verified");
+
+  @override
+  void initState() {
+    super.initState();
+    pb.collection('users').requestVerification(widget.email);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Verify your email"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: FutureBuilder(
+        future: isVerified, builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            //here double check it hasn't been verified yet
+            return TellToVerifyEmail(email: widget.email,);
+          } else if (snapshot.hasError) {
+            return AlertDialog(
+              title: const Text("Something went wrong :/"),
+              content: Text(snapshot.error.toString()),
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator(),);
+          }
+        }),
+    );
+  }
+}
+
+class TellToVerifyEmail extends StatelessWidget {
+  const TellToVerifyEmail({super.key, required this.email});
+  final String email;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text("Please click the link sent to $email"),
+            ElevatedButton(onPressed: () async {
+        
+            final record = await pb.collection('users').getOne(pb.authStore.model.id, fields: "verified");
+            if (record.data['verified']) {
+              Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => const HomePage()), (route) => false);
+            } else {
+              showDialog(context: context, 
+              builder: (context) => AlertDialog(
+                title: const Text("Your email isn't verified"),
+                content: const Text("Click the link sent to your email, or click the < button at the top left of this page to type a different email"),
+                actions: [TextButton(onPressed: () => Navigator.pop(context), child: const Text("OK"),)],
+              ));
+            }
+            }, child: const Text("Done"))
+          ],
+        ),
+      ),
+    );
+  }
+}
