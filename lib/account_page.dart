416efd1858e02eec30be4cdd20503d6a0a483b9f
Austin <105258719+Austin-540@users.noreply.github.com>
2024-04-05 09:52:13 +1300

    Account page update

--- lib/account_page.dart
+++ lib/account_page.dart
@@ -4,8 +4,17 @@ import 'package:shc_cricket_bookings/login_page.dart';
 import 'globals.dart';
 import 'package:flutter/material.dart';
 
-class AccountPage extends StatelessWidget {
+class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
+
+  @override
+  State<AccountPage> createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
+  Future accountData = pb.collection('users').getOne(pb.authStore.model.id,
+  expand: 'permissions',
+);
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +27,26 @@ class AccountPage extends StatelessWidget {
         Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => LoginPage()), (route) => false);
 
       }, label: Text("Logout"), icon: Icon(Icons.logout),),
-    );
+
+      body: FutureBuilder(
+        future: accountData,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                Text(snapshot.data.toString())
+              ],
+            );
+          } else if (snapshot.hasError) {
+            return Text("Something went wrong getting your account data.\n${snapshot.error.toString()}");
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+        
+      ),);
+    
   }
 }
