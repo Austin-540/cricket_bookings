d0eddb205dfa84f3a9b6f9a403d19dbf749efb0d
Austin <105258719+Austin-540@users.noreply.github.com>
2024-05-23 12:41:27 +1200

    make each timeslot customisable

--- lib/booking_page.dart
+++ lib/booking_page.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+
+class TimeSlot {
+  TimeSlot({required this.startTime, required this.endTime, required this.booked});
+    final int startTime;
+    final int endTime;
+    final bool booked;
+}
+
 class BookingPage extends StatefulWidget {
   BookingPage({super.key, required this.selected});
   bool selected;
@@ -11,7 +19,8 @@ class BookingPage extends StatefulWidget {
 
 class _BookingPageState extends State<BookingPage> {
   DateTime? datePicked = DateTime.now();
-  
+  final timeslots = [TimeSlot(startTime: 9, endTime: 10, booked: false)];
+
   @override
   Widget build(BuildContext context) {
     if (!widget.selected) {
@@ -27,12 +36,38 @@ class _BookingPageState extends State<BookingPage> {
       body: ListView(
         children: [
           ElevatedButton(onPressed: () async{
-            DateTime? datePickerPicked = await showDatePicker(context: context, firstDate: DateTime.now(), lastDate: DateTime(2085));
+            DateTime? datePickerPicked = await showDatePicker(
+              context: context, firstDate: DateTime.now(), lastDate: DateTime(2085));
             setState(() {
               datePicked = datePickerPicked ?? DateTime.now();
             });
           }, child: const Text("Pick a different date")),
-          Text("Date selected: $datePicked")
+          Text("Date selected: $datePicked"),
+          for (var i=0; i< timeslots.length; i++) ... [
+              Card(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Row(children: [
+                Text("${timeslots[i].startTime} - ${timeslots[i].endTime}", style: const TextStyle(fontSize: 40),),
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    children: [
+                      Checkbox(
+                        
+                        value: false, onChanged: timeslots[i].booked? null:(value){
+
+                      } ,),
+                      timeslots[i].booked? const Text("Booked"): const Text("Available")
+                    ],
+                  ),
+                )
+              ],),
+            ),
+          )
+          ],
+          
         ],
         ),
     );
