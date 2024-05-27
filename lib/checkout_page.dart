a9829d7c3470c0cb571a4a37058ddebe12dafe51
Austin <105258719+Austin-540@users.noreply.github.com>
2024-05-27 13:51:43 +1200

    basic layout for the checkout page

--- lib/checkout_page.dart
+++ lib/checkout_page.dart
@@ -1,8 +1,9 @@
+import 'booking_page.dart';
 import 'package:flutter/material.dart';
 
 class CheckoutPage extends StatefulWidget {
   const CheckoutPage({super.key, required this.timeslots});
-  final List timeslots;
+  final List<TimeSlot> timeslots;
 
   @override
   State<CheckoutPage> createState() => _CheckoutPageState();
@@ -12,8 +13,16 @@ class _CheckoutPageState extends State<CheckoutPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-        body: Text(widget.timeslots.toString()),
+      appBar: AppBar(title: Text("Checkout"),),
+        body: Column(children: [
+          for (int i = 0; i < widget.timeslots.length; i++) ... [
+            Text("${widget.timeslots[i].startTime} - ${widget.timeslots[i].endTime} ${widget.timeslots[i].am_or_pm}"),
+          ],
+            Text("Cost goes here"),
+            OutlinedButton(onPressed: (){
+              //code for making a booking goes here
+            }, child: Text("Book it"))
+        ],),
     );
   }
 }
