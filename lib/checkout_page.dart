32a4e6daa97eb8536df035378446bb6107c68923
Austin <105258719+Austin-540@users.noreply.github.com>
2024-06-10 10:01:22 +1200

    basic minimum checkout page

--- lib/checkout_page.dart
+++ lib/checkout_page.dart
@@ -13,15 +13,17 @@ class _CheckoutPageState extends State<CheckoutPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Checkout"),),
+      appBar: AppBar(title: Text("Checkout"), backgroundColor: Theme.of(context).colorScheme.inversePrimary,),
         body: Column(children: [
           for (int i = 0; i < widget.timeslots.length; i++) ... [
-            Text("${widget.timeslots[i].startTime} - ${widget.timeslots[i].endTime} ${widget.timeslots[i].am_or_pm}"),
+            Text("${widget.timeslots[i].startTime} - ${widget.timeslots[i].endTime} ${widget.timeslots[i].am_or_pm}", 
+            style: TextStyle(fontSize: 50),),
           ],
-            Text("Cost goes here"),
+            Text("Cost goes here",
+            style: TextStyle(fontSize: 30),),
             OutlinedButton(onPressed: (){
               //code for making a booking goes here
-            }, child: Text("Book it"))
+            }, child: Text("Book it")) 
         ],),
     );
   }
