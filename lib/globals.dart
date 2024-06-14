78340956d1b02e7bcaa2f729b732d51982823f2e
Austin <105258719+Austin-540@users.noreply.github.com>
2024-06-14 15:13:24 +1200

    add reusable error Dialog

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,3 +1,19 @@
 import 'package:pocketbase/pocketbase.dart';
+import 'package:flutter/material.dart';
 
 final pb = PocketBase("https://pb.shccricket.com/");
+
+
+class ErrorDialog extends StatelessWidget {
+  const ErrorDialog({super.key, required this.error});
+  final error;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text("Something went wrong"),
+      content: Text(error.toString()),
+      actions: [TextButton(onPressed: () => Navigator.pop(context), child: const Text("OK"))],
+    );
+  }
+}
